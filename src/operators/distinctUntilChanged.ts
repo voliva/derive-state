35da4143570aaa1a6b375221a6003b000aa600d6
Víctor Oliva <olivarra1@gmail.com>
2021-01-26 16:39:07 +0100

    add equality function to distinctUntilChanged

--- src/operators/distinctUntilChanged.ts
+++ src/operators/distinctUntilChanged.ts
@@ -1,11 +1,13 @@
 import { DerivedState } from '../state';
 import { Observable } from '../interface';
 
-export const distinctUntilChanged = <T>() => (source: Observable<T>) =>
-  new DerivedState<T>(next => {
+export const distinctUntilChanged = <T>(
+  eqFn: (a: T, b: T) => boolean = (a, b) => a === b
+) => (source: Observable<T>) =>
+  new DerivedState<T>((next) => {
     let lastValue: T | typeof EMPTY = EMPTY;
-    return source.subscribe(value => {
-      if (value !== lastValue) {
+    return source.subscribe((value) => {
+      if (lastValue === EMPTY || !eqFn(value, lastValue)) {
         next(value);
       }
       lastValue = value;
